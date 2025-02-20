c9030ac680506e7f39808ef6462ffe5396ca8103
DharmShah <gautamdharm1511@gmail.com>
2025-02-20 15:33:23 +0530

    pages added to th∈project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Signin from './pages/Signin';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello Dharm</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
