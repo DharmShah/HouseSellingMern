338ec164a2e7af8061246295b8c0628b8c6ad75b
DharmShah <gautamdharm1511@gmail.com>
2025-02-20 23:37:18 +0530

    MongoDb Colud Connection SetUp Creating .env file successfully

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log("Mongo URI:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("Database connection error:", err));
 
 const app = express();
 
-app.listen(3000,() =>{
-    console.log("Server is running on port 3000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
