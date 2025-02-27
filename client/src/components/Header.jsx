58830319bd356fe067b75ddc522b2d72c474bce1
DharmShah <gautamdharm1511@gmail.com>
2025-02-27 14:41:55 +0530

    privated profile page and added profile icon to the header when the user is signin

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 export default function Header() {
+  const {currentUser}=useSelector(state => state.user)
   return (
     <div>
       <header className='bg-slate-200 shadow-md'>
@@ -20,7 +22,15 @@ export default function Header() {
         <ul className='flex gap-4  '>
             <Link to='/' ><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
             <Link to='/about' ><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-            <Link to='/sign-up'><li className='text-slate-700 hover:underline' >Sign Up</li></Link>
+
+            <Link to='/profile'>
+              {currentUser?(
+                <img className='rounded-full h-8 w-8 object-cover' src={currentUser.avatar} alt="profile" />
+              ):(
+              <li className='text-slate-700 hover:underline' >Sign Up</li>
+                 )}
+                </Link>
+
         </ul>
         </div>
       </header>
